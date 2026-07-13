290fd55b8b9cfc9685c2cf787123eef50faf2821
Andik67 <andymohammadhafizz@gmail.com>
2026-07-13 08:04:56 +0800

    lab4-3 finish

---
+++ labs/lab04/exercise3/lab-4-3.py
@@ -0,0 +1,11 @@
+hours = int(input())
+if hours > 2:
+    if hours > 5:
+        charges = hours - 5 * 3
+    else:
+        charges = hours - 3 * 2
+else:
+    charges = 0
+if charges > 30:
+    parkingFee = 30
+print(parkingFee)
