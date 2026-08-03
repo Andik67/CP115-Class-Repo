94c985fcdf0d95711ca327d9fc5df9becb2be998
Andik67 <andymohammadhafizz@gmail.com>
2026-08-03 09:07:15 +0800

    Change the print in Lab 1

---
+++ labs/lab05/exercise2.py
@@ -0,0 +1,4 @@
+Name = input("What is your name? : ")
+Age = input("How old are you? : ")
+Course_code = input("What is your course code? :")
+print("Your name is " + Name + ", Your age is " + str(Age) + " and your course code is " + str(Course_code))
