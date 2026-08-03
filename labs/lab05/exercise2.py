fb06bb1af2232c3de24bd5a02af8abc97f7d4852
Andik67 <andymohammadhafizz@gmail.com>
2026-08-03 09:31:45 +0800

    complete lab5 exercise 2

---
+++ labs/lab05/exercise2.py
@@ -0,0 +1,6 @@
+import math
+Radius = int(input("Masukkan nilai radius: "))
+Area_circle = math.pi * Radius * Radius
+Circumference_circle = 2 * math.pi * Radius
+print(Area_circle)
+print(Circumference_circle)
